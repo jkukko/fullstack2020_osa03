97ec6cb4a0c111efd6f7c85da3940a349247155f
jkukko <joonas.kukkonen@gmail.com>
2020-09-06 22:35:00 +0300

    Exercise 3.6 done

--- index.js
+++ index.js
@@ -69,6 +69,25 @@ const generateId = () => {
 
 app.post('/api/persons', (request, response) => {
     const body = request.body
+
+    if (!body.name) {
+        return response.status(400).json({
+            error: 'name missing'
+        })
+    }
+
+    if (!body.number) {
+        return response.status(400).json({
+            error: 'number missing'
+        })
+    }
+
+    if (persons.find(person => person.name === body.name)) {
+        return response.status(400).json({
+            error: 'name must be unique'
+        })
+    }
+
     const person = {
         name: body.name,
         number: body.number,
