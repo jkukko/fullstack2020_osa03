31cb0a9262a855416b31b8f8af68a08ca45601f7
jkukko <joonas.kukkonen@gmail.com>
2020-09-24 22:17:33 +0300

    Exercise 3.11

---
+++ build/precache-manifest.fec4a380a69589109b42959ecf876931.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2932982581006493add255081a90a0e0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3df0ca1dd853370b3f7a",
+    "url": "/static/css/main.613d21e3.chunk.css"
+  },
+  {
+    "revision": "740e955edf9b73a83760",
+    "url": "/static/js/2.584d881a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.584d881a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3df0ca1dd853370b3f7a",
+    "url": "/static/js/main.94b9f7a6.chunk.js"
+  },
+  {
+    "revision": "f0be2d6b237b3fb64219",
+    "url": "/static/js/runtime-main.24b8e7b7.js"
+  }
+]);
